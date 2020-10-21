1e57c2264d631329caae2b3c5246400f84428203
Dave Mellert <Dave.Mellert@jax.org>
2020-10-21 10:45:19 -0400

    Create a new fixture for adding users and groups to the test instance
    rough working concept
    
    working draft users_groups fixture and reorg
    
    cleaned up tests
    
    working fixture

--- jax_omeroutils/tests/test_ezomero.py
+++ jax_omeroutils/tests/test_ezomero.py
@@ -1,113 +1,6 @@
 import pytest
 import numpy as np
-from datetime import datetime
 from jax_omeroutils import ezomero
-from omero.gateway import BlitzGateway
-from omero.gateway import ScreenWrapper, PlateWrapper
-from omero.model import ScreenI, PlateI, WellI, WellSampleI, ImageI
-from omero.model import ScreenPlateLinkI
-from omero.rtypes import rint
-
-
-# we can change this later
-@pytest.fixture(scope="session")
-def omero_params(request):
-    user = request.config.getoption("--omero-user")
-    password = request.config.getoption("--omero-pass")
-    host = request.config.getoption("--omero-host")
-    port = request.config.getoption("--omero-port")
-    secure = request.config.getoption("--omero-secure")
-    return(user, password, host, port, secure)
-
-
-@pytest.fixture(scope='session')
-def conn(omero_params):
-    user, password, host, port, secure = omero_params
-    conn = BlitzGateway(user, password, host=host, port=port, secure=secure)
-    conn.connect()
-    yield conn
-    conn.close()
-
-
-@pytest.fixture(scope='session')
-def image_fixture():
-    test_image = np.zeros((200, 201, 20, 3, 1), dtype=np.uint8)
-    test_image[0:100, 0:100, 0:10, 0, :] = 255
-    test_image[0:100, 0:100, 11:20, 1, :] = 255
-    test_image[101:200, 101:201, :, 2, :] = 255
-    return test_image
-
-
-@pytest.fixture(scope='session')
-def timestamp():
-    return f'{datetime.now():%Y%m%d%H%M%S}'
-
-
-@pytest.fixture(scope='session')
-def project_structure(conn, timestamp, image_fixture):
-    """
-    Project              Dataset           Image
-    -------              -------           -----
-    proj   ---->    ds    ---->            im0
-
-    Screen        Plate         Well          Image
-    ------        -----         ----          -----
-    screen ---->  plate ---->   well   ----->  im1
-    """
-
-    proj_name = "proj_" + timestamp
-    proj_id = ezomero.post_project(conn, proj_name)
-
-    ds_name = "ds_" + timestamp
-    ds_id = ezomero.post_dataset(conn, ds_name,
-                                 project_id=proj_id)
-
-    im_name = 'im_' + timestamp
-    im_id = ezomero.post_image(conn, image_fixture, im_name,
-                               dataset_id=ds_id)
-
-    update_service = conn.getUpdateService()
-
-    # Create Screen
-    screen_name = "screen_" + timestamp
-    screen = ScreenWrapper(conn, ScreenI())
-    screen.setName(screen_name)
-    screen.save()
-    screen_id = screen.getId()
-
-    # Create Plate
-    plate_name = "plate_" + timestamp
-    plate = PlateWrapper(conn, PlateI())
-    plate.setName(plate_name)
-    plate.save()
-    plate_id = plate.getId()
-    link = ScreenPlateLinkI()
-    link.setParent(ScreenI(screen_id, False))
-    link.setChild(PlateI(plate_id, False))
-    update_service.saveObject(link)
-
-    # Create Well
-    well = WellI()
-    well.setPlate(PlateI(plate_id, False))
-    well.setColumn(rint(1))
-    well.setRow(rint(1))
-    well.setPlate(PlateI(plate_id, False))
-
-    # Create Well Sample with Image
-    ws = WellSampleI()
-    im_id1 = ezomero.post_image(conn, image_fixture, "well image")
-    ws.setImage(ImageI(im_id1, False))
-    well.addWellSample(ws)
-    well_obj = update_service.saveAndReturnObject(well)
-    well_id = well_obj.getId().getValue()
-
-    return({'proj': proj_id,
-            'ds': ds_id,
-            'im': im_id,
-            'screen': screen_id,
-            'plate': plate_id,
-            'well': well_id,
-            'im1': im_id1})
 
 
 def test_omero_connection(conn, omero_params):
@@ -116,7 +9,6 @@ def test_omero_connection(conn, omero_params):
 
 # Test posts
 ############
-
 def test_post_dataset(conn, project_structure, timestamp):
     # Orphaned dataset, with descripion
     ds_test_name = 'test_post_dataset_' + timestamp
