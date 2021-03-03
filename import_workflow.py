4ce9d687ce134db9111815c47aa7993f446bc27e
Erick Martins Ratamero <erickmartins@gmx.net>
2021-03-03 17:15:18 -0500

    debug print

--- import_workflow.py
+++ import_workflow.py
@@ -18,9 +18,9 @@ def demote(user_uid, user_gid, homedir):
 
 
 def retrieve_json(stdoutval):
+    if (stdoutval == "\n"):
+        print("empty stdout")
     last_line = stdoutval.split('\n')[-2]
-    if (last_line == "\n"):
-        print("empty last line")
     json_path = last_line.split(':')[-1].strip()
     return json_path
 
