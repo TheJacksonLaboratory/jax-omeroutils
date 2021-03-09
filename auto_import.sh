72e3fb28ecacc27b4008480c12c9939b69ea3fee
Erick Martins Ratamero <erickmartins@gmx.net>
2021-03-09 15:19:12 -0500

    created auto_import.sh

---
+++ auto_import.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+usage="$(basename "$0") [-h] [-e exclude ] target_folder [optional arguments] -- runs imports on all subfolders from the argument folder.
+where:
+    -h  show this help text
+    -e  Passes a list of folders to be excluded from import
+
+    Any optional arguments passed after the target folder are passed through to import_workflow.py. 
+    This script will run whatever is in your \$IMPORT env variable over the immediate subfolders of your target folder.
+
+"
+
+##Default options in case user doesn't pass any arguments
+exclude=""
+folder=""
+arguments=""
+
+##Parse arguments
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -e|--exclude) exclude="$2"; shift 2;;
+    
+    -h) echo "$usage"; exit 0;;
+    -*) echo "unknown option: $1" >&2; exit 1;;
+    **) break;;
+  esac
+
+done
+folder="$1"; shift 1;
+arguments="$*"
+for dir in $folder/*/; do 
+    skip=false
+    for exc in $(cat $exclude); do
+        if [ "$(realpath $exc)" == "$(realpath $dir)" ] ; then
+            echo "Skipping" $dir " - directory in exclude file";
+            skip=true
+        fi
+    done
+    if [ "$skip" = false ]; then
+        echo "Processing" $dir;
+        $IMPORT $dir $arguments; 
+    fi
+    
+done
+
