df0e428512e7dfea6ba8fe6b968f16305853e3d8
Erick Martins Ratamero <erickmartins@gmx.net>
2021-05-10 14:54:39 -0400

    uses data user to run read-only queries on import folders

--- auto_import.sh
+++ auto_import.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -l
 
 
-usage="$(basename "$0") [-h] [-e exclude ] target_folder [optional arguments] -- runs imports on all subfolders from the argument folder.
+usage="$(basename "$0") [-h] [-e exclude -u datauser] target_folder [optional arguments] -- runs imports on all subfolders from the argument folder.
 where:
     -h  show this help text
     -e  Passes a list of folders to be excluded from import
+    -u  Specifies a user with read permissions to data folders
 
     Any optional arguments passed after the target folder are passed through to import_workflow.py. 
     This script will run whatever is in your \$IMPORT env variable over the immediate subfolders of your target folder.
@@ -15,12 +16,13 @@ where:
 exclude=""
 folder=""
 arguments=""
+user="svc-omerodata"
 
 ##Parse arguments
 while [ "$#" -gt 0 ]; do
   case "$1" in
     -e|--exclude) exclude="$2"; shift 2;;
-    
+    -u|--user) user="$2"; shift 2;;
     -h) echo "$usage"; exit 0;;
     -*) echo "unknown option: $1" >&2; exit 1;;
     **) break;;
@@ -36,7 +38,7 @@ cd "$(dirname "$0")"
 
 arguments="$*"
 # check for folders last modified more than 60 mins ago
-for dir in $(find $folder -mindepth 1 -maxdepth 1 -type d -mmin +60); do 
+for dir in $(sudo -u $user find $folder -mindepth 1 -maxdepth 1 -type d -mmin +60); do 
 
     skip=false
     if [ "$exclude" ]; then
@@ -52,7 +54,7 @@ for dir in $(find $folder -mindepth 1 -maxdepth 1 -type d -mmin +60); do
         # check whether email will need to be sent
         email=false
         # time cutoff is 60 mins + gap between cron runs (in our case, 360 mins)
-        modified=$(find $dir -mindepth 1 -maxdepth 1 -type f -cmin -420 | wc -l)
+        modified=$(sudo -u $user find $dir -mindepth 1 -maxdepth 1 -type f -cmin -420 | wc -l)
         echo "Folder has $modified modified files since last cron run."
         if [ $modified -gt 1 ]; then
             email=true
@@ -64,13 +66,13 @@ for dir in $(find $folder -mindepth 1 -maxdepth 1 -type d -mmin +60); do
 
         #check whether folder is "empty" now
         empty=false
-        allfiles=$(find $dir -mindepth 1 -maxdepth 1 -type f | wc -l)
-        nonimages=$(find $dir -mindepth 1 -maxdepth 1 -regex ".*\.\(xlsx\|csv\|log\|json\|db\|ini\|txt\)" -type f | wc -l)
+        allfiles=$(sudo -u $user find $dir -mindepth 1 -maxdepth 1 -type f | wc -l)
+        nonimages=$(sudo -u $user find $dir -mindepth 1 -maxdepth 1 -regex ".*\.\(xlsx\|csv\|log\|json\|db\|ini\|txt\)" -type f | wc -l)
         echo "Folder has $allfiles files left, of which $nonimages are typical non-images."
         if [ $allfiles -eq $nonimages ]; then
             empty=true
         fi
-        logfile=$(find $dir -mindepth 1 -maxdepth 1 -regex ".*\.\(log\)" -type f -mmin -10)
+        logfile=$(sudo -u $user find $dir -mindepth 1 -maxdepth 1 -regex ".*\.\(log\)" -type f -mmin -10)
         empty_msg=""
         # add message for empty folder
         if [ "$empty" = true ]; then
@@ -92,7 +94,7 @@ for dir in $(find $folder -mindepth 1 -maxdepth 1 -type d -mmin +60); do
                                 echo "From: noreply-omero-importer@jax.org" && \
                                 echo "Subject: omero import log for folder $email_dir" && \
                                 echo "" && \
-                                cat $logfile && \
+                                sudo -u $user cat $logfile && \
                                 echo "$empty_msg"
                                 ) > $HOME/temp_email.txt
             ssmtp $address < $HOME/temp_email.txt
