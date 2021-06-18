81ecf3a2a10e63f1bf3ef05e322a49416f3077fa
Erick Martins Ratamero <erickmartins@gmx.net>
2021-06-18 10:43:23 -0400

    Hypercare alterations (#16)
    * minor changes to autoimport and making nometadata-file errors more informative
    
    * improved parsing and dealing with rogue spaces
    
    * stopping rogue quotes
    
    * added a few debug things
    
    * sudoing is necessary checking logfile
    
    * sudo on the actual if
    
    * moved to using basename for dir and sending email to folder owner as well
    
    * adding delay after emailing to avoid rate issues
    
    * removing sleeps cause they don't work
    
    * if email sending fails, retry (up to maximum number of tries)
    
    * protecting against changes on user and group keys

--- auto_import.sh
+++ auto_import.sh
@@ -17,6 +17,7 @@ exclude=""
 folder=""
 arguments=""
 user="svc-omerodata"
+maxtries=10
 
 ##Parse arguments
 while [ "$#" -gt 0 ]; do
@@ -44,13 +45,13 @@ for dir in $(sudo -u $user find $folder -mindepth 1 -maxdepth 1 -type d -mmin +6
     if [ "$exclude" ]; then
         for exc in $(cat $exclude); do
             if [ "$(realpath $exc)" == "$(realpath $dir)" ] ; then
-                echo "Skipping" $dir " - directory in exclude file";
+                echo "Skipping" "$dir" " - directory in exclude file";
                 skip=true
             fi
         done
     fi
     if [ "$skip" = false ]; then
-        echo "Processing" $dir;
+        echo "Processing" "$dir";
         # check whether email will need to be sent
         email=false
         # time cutoff is 60 mins + gap between cron runs (in our case, 360 mins)
@@ -59,10 +60,10 @@ for dir in $(sudo -u $user find $folder -mindepth 1 -maxdepth 1 -type d -mmin +6
         if [ $modified -gt 1 ]; then
             email=true
         fi
-
+        echo "Will I send an email? $email"
         # actually do imports
         
-        $IMPORT $dir $arguments; 
+        $IMPORT "$dir" $arguments; 
 
         #check whether folder is "empty" now
         empty=false
@@ -81,13 +82,38 @@ for dir in $(sudo -u $user find $folder -mindepth 1 -maxdepth 1 -type d -mmin +6
             echo "(that does NOT mean that all files in your SPREADSHEET were imported; it is your" && \
             echo -e "responsibility to check all files from your submission were present in the folder.)\n" )
         fi
+        echo "logfile should be at $logfile"
+        if sudo -u $user [ -f "$logfile" ]; then
+            echo "I have found the logfile"
+        fi
 
         # send email if necessary
-        if [ "$email" = true -a -f $logfile ]; then
+        if [ "$email" = true ] && sudo -u $user [ -f "$logfile" ]; then
             #retrieve email of user by splitting dir name on underscore
-            address=$(echo ${dir##*/} | cut -f1 -d_)"@jax.org"
+            address=$(basename $dir | cut -f1 -d_)"@jax.org"
+            address_owner=$(find $dir -maxdepth 0 -printf '%u\n')"@jax.org"
             echo "Sending email to user $address"
-            email_dir=${dir##*/}
+            email_dir=$(basename $dir)
+            if [ $address_owner != $address ]; then
+                echo "Also sending email to user $address_owner"
+                (
+                                echo "To: $address_owner" && \
+                                echo "From: noreply-omero-importer@jax.org" && \
+                                echo "Subject: omero import log for folder $email_dir" && \
+                                echo "" && \
+                                sudo -u $user cat $logfile && \
+                                echo "$empty_msg"
+                                ) > "$HOME"/temp_email_owner.txt
+                ssmtp $address_owner < "$HOME"/temp_email_owner.txt
+                err=$?
+                tries=1
+                while [ $err -ne 0 ] && [ $tries -le $maxtries ]; do 
+                    ssmtp $address_owner < "$HOME"/temp_email_owner.txt 
+                    err=$? 
+                    ((tries+=1))
+                done
+            fi
+            
             #send email
             (
                                 echo "To: $address" && \
@@ -96,12 +122,16 @@ for dir in $(sudo -u $user find $folder -mindepth 1 -maxdepth 1 -type d -mmin +6
                                 echo "" && \
                                 sudo -u $user cat $logfile && \
                                 echo "$empty_msg"
-                                ) > $HOME/temp_email.txt
-            ssmtp $address < $HOME/temp_email.txt
-
+                                ) > "$HOME"/temp_email.txt
+            ssmtp $address < "$HOME"/temp_email.txt
+            err=$?
+            tries=1
+            while [ $err -ne 0 ] && [ $tries -le $maxtries ]; do 
+                ssmtp $address_owner < "$HOME"/temp_email_owner.txt 
+                err=$? 
+                ((tries+=1))
+            done
         fi
     fi
-
-    
 done
 
--- jax_omeroutils/intake.py
+++ jax_omeroutils/intake.py
@@ -118,10 +118,23 @@ def load_md_from_file(md_filepath, sheet_name=0):
                        skiprows=range(4),
                        dtype=str,
                        engine="openpyxl")
+    
+    # fixing rogue lines, rogue columns, rogue leading/trailing spaces in any string column
+    md = md.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+    md = md.dropna(subset=['filename', 'project', 'dataset']).dropna(axis='columns', how='all')
+
+    # protect against extra spaces on 'omero user' and 'omero group'
+    md_header.index = md_header.index.str.strip()
     md_json = {}
-    md_json['omero_user'] = md_header.loc['OMERO user:', 1]
-    md_json['omero_group'] = md_header.loc['OMERO group:', 1]
-    md_json['file_metadata'] = md.to_dict(orient='records')
+    try:
+        # added 'strip' to protect against leading/trailing spaces
+        md_json['omero_user'] = md_header.loc['OMERO user:', 1].strip()
+        md_json['omero_group'] = md_header.loc['OMERO group:', 1].strip()
+        md_json['file_metadata'] = md.to_dict(orient='records')
+    except KeyError:
+        logger.error("Your spreadsheet does not have 'OMERO user:' or 'OMERO group:' fields, or you \
+        have added your username/group on the same cell as those fields! Please add them on column B.")
+        raise KeyError(f"User and group fields are non-existent or malformed.")
     return(md_json)
 
 # Class definitions #
