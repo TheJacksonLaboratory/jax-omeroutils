b6846dd92b0dc2f94ca39f9f3ffd40ce4380867b
Erick Martins Ratamero <erickmartins@gmx.net>
2023-07-18 14:29:32 -0400

    checking json columns

--- jax_omeroutils/xml_editor.py
+++ jax_omeroutils/xml_editor.py
@@ -1,10 +1,33 @@
+import copy
+
+
 def add_projects_datasets(ome, imp_json):
-    return ome
+    newome = copy.deepcopy(ome)
+    columns = imp_json['user_supplied_md']['file_metadata'][0].keys()
+    if 'project' not in columns:
+        return ome
+    else:
+        print(columns)
+    return newome
 
 
 def add_screens(ome, imp_json):
-    return ome
+    newome = copy.deepcopy(ome)
+    columns = imp_json['user_supplied_md']['file_metadata'][0].keys()
+    if 'screen' not in columns:
+        return ome
+    else:
+        print(columns)
+    return newome
 
 
 def add_annotations(ome, imp_json):
-    return ome
+    newome = copy.deepcopy(ome)
+    columns = imp_json['user_supplied_md']['file_metadata'][0].keys()
+    if 'project' in columns:
+        columns.remove('project')
+    if 'dataset' in columns:
+        columns.remove('dataset')
+    if 'screen' in columns:
+        columns.remove('screen')
+    return newome
