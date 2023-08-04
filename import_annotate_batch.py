3cad6b4059759094b1381c49024a9c2fe8a896b5
Erick Martins Ratamero <erickmartins@gmx.net>
2023-08-04 14:12:41 -0400

    minimal unpack usage

--- import_annotate_batch.py
+++ import_annotate_batch.py
@@ -10,6 +10,7 @@ from jax_omeroutils.config import OMERO_HOST, OMERO_PORT
 from jax_omeroutils.importer import Importer
 from omero.gateway import BlitzGateway
 from pathlib import Path
+import subprocess
 
 
 def main(import_md):
@@ -20,44 +21,59 @@ def main(import_md):
 
     import_user = batch_md['user']
     import_group = batch_md['group']
-    file_metadata = batch_md['import_targets']
+    # file_metadata = batch_md['import_targets']
     data_dir = Path(batch_md['server_path'])
 
-    # Create user connection
-    suconn = BlitzGateway(OMERO_USER,
-                          OMERO_PASS,
-                          host=OMERO_HOST,
-                          port=OMERO_PORT,
-                          secure=True)
-    suconn.connect()
-    conn = suconn.suConn(import_user, import_group, 2160000000)
-    suconn.close()
+    unpack = ['omero', '-s', OMERO_HOST, '-p', str(OMERO_PORT),
+               '-u', import_user, '-w', OMERO_PASS, '-g', import_group,
+               '--sudo', OMERO_USER,
+               'transfer', 'unpack', data_dir]
+    process = subprocess.Popen(unpack,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE
+                               )
+    stdoutval, stderrval = process.communicate()
+    stdoutval, stderrval = stdoutval.decode('UTF-8'), stderrval.decode('UTF-8')
+    print("stdout unpack:", stdoutval)
+    print("stderr unpack:", stderrval)
+
+
+    # # Create user connection
+    # suconn = BlitzGateway(OMERO_USER,
+    #                       OMERO_PASS,
+    #                       host=OMERO_HOST,
+    #                       port=OMERO_PORT,
+    #                       secure=True)
+    # suconn.connect()
+    # conn = suconn.suConn(import_user, import_group, 2160000000)
+    # suconn.close()
     
-    # do I need a session key for conn? can I pass --sudo on transfer itself?
+    # # do I need a session key for conn? can I pass --sudo on transfer itself?
+    # # you should be able to pass --sudo on transfer!
 
 
-    # Import targets from import.json
-    for md in file_metadata:
-        filename = md['filename']
-        file_path = data_dir / filename
-        print(f'Preparing to import {str(file_path)}')
-        imp_ctl = Importer(conn, file_path, md)
-        imp_ctl.import_ln_s(OMERO_HOST, OMERO_PORT)
-        print("import done")
-        if imp_ctl.screen:
-            print("it's a screen")
-            imp_ctl.get_plate_ids()
-            print(f"plate ids get! {str(imp_ctl.plate_ids[0])}")
-            imp_ctl.organize_plates()
-            print("plate organized")
-            imp_ctl.annotate_plates()
-            print("plate annotated")
-        else:
-            imp_ctl.get_image_ids()
-            imp_ctl.organize_images()
-            imp_ctl.annotate_images()
+    # # Import targets from import.json
+    # for md in file_metadata:
+    #     filename = md['filename']
+    #     file_path = data_dir / filename
+    #     print(f'Preparing to import {str(file_path)}')
+    #     imp_ctl = Importer(conn, file_path, md)
+    #     imp_ctl.import_ln_s(OMERO_HOST, OMERO_PORT)
+    #     print("import done")
+    #     if imp_ctl.screen:
+    #         print("it's a screen")
+    #         imp_ctl.get_plate_ids()
+    #         print(f"plate ids get! {str(imp_ctl.plate_ids[0])}")
+    #         imp_ctl.organize_plates()
+    #         print("plate organized")
+    #         imp_ctl.annotate_plates()
+    #         print("plate annotated")
+    #     else:
+    #         imp_ctl.get_image_ids()
+    #         imp_ctl.organize_images()
+    #         imp_ctl.annotate_images()
 
-    conn.close()
+    # conn.close()
     return
 
 
