852ca959524dd346b5760b626e0d37ecfd8f131f
Erick Martins Ratamero <erickmartins@gmx.net>
2023-08-16 13:38:14 -0400

    added debug print

--- jax_omeroutils/intake.py
+++ jax_omeroutils/intake.py
@@ -479,6 +479,7 @@ class ImportTarget:
 
         cli = CLI()
         cli.register('import', ImportControl, '_')
+        print(self.path_to_target)
         cli.invoke(['import', '-f', str(self.path_to_target)])
 
         if cli.rv == 0:
